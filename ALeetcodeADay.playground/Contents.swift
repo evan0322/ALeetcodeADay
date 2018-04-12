f35118052e8597c0537ff15fbbe5579944aff0b9
evan0322 <evan201314@gmail.com>
2018-04-11 23:28:51 -0400

    416. Partition Equal Subset Sum

--- ALeetcodeADay.playground/Contents.swift
+++ ALeetcodeADay.playground/Contents.swift
@@ -580,6 +580,46 @@ class Solution {
         
         return find ? highIndex - lowIndex + 1 : 0
     }
+    
+    //416. Partition Equal Subset Sum
+    /*
+    This problem is essentially let us to find whether there are several numbers in a set which are able to sum to a specific value (in this problem, the value is sum/2).
+    
+    Actually, this is a 0/1 knapsack problem, for each number, we can pick it or not. Let us assume dp[i][j] means whether the specific sum j can be gotten from the first i numbers. If we can pick such a series of numbers from 0-i whose sum is j, dp[i][j] is true, otherwise it is false.
+    
+    Base case: dp[0][0] is true; (zero number consists of sum 0 is true)
+    
+    Transition function: For each number, if we don’t pick it, dp[i][j] = dp[i-1][j], which means if the first i-1 elements has made it to j, dp[i][j] would also make it to j (we can just ignore nums[i]). If we pick nums[i]. dp[i][j] = dp[i-1][j-nums[i]], which represents that j is composed of the current value nums[i] and the remaining composed of other previous numbers. Thus, the transition function is dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
+     */
+    func canPartition(_ nums: [Int]) -> Bool {
+        var sum = 0
+        for num in nums {
+            sum += num
+        }
+        
+        if sum%2 != 0 {
+            return false
+        }
+        
+        sum = sum/2
+        
+        var dp = Array(repeating:Array(repeating:false, count:sum + 1), count: nums.count)
+        
+        for i in 1..<nums.count {
+            dp[i][0] = true
+        }
+        
+        dp[0][0] = true
+        
+        for i in 1..<nums.count {
+            for j in 1...sum {
+                if j >= nums[i] {
+                    dp[i][j] = dp[i - 1][j]||dp[i - 1][j - nums[i]]
+                }
+            }
+        }
+        return dp[nums.count - 1][sum]
+    }
 }
 
 
@@ -640,7 +680,7 @@ func placeChess(placedChessses:[chess], c: Int) {
 //}
 
 
-//Here is the solustion with memo. complexity Wn 
+//Here is the solustion with memo. complexity Wn
 func knapsack(W:Int,ws:[Int],vs:[Int]) -> Int {
     var memo = Array(repeating: Array(repeating:-1, count:ws.count), count: W + 1)
     
