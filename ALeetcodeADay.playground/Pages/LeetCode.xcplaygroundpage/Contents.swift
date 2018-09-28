9831af90a7a7f8eca1ca811a3927efb1749b7461
evan0322 <evan201314@gmail.com>
2018-09-27 23:33:58 -0400

    26. Remove Duplicates from Sorted Array

--- ALeetcodeADay.playground/Pages/LeetCode.xcplaygroundpage/Contents.swift
+++ ALeetcodeADay.playground/Pages/LeetCode.xcplaygroundpage/Contents.swift
@@ -2440,6 +2440,25 @@ class Solution {
         
     }
     
+    //26. Remove Duplicates from Sorted Array
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        guard nums.count > 1 else {
+            return nums.count
+        }
+        
+        var i = 0
+        
+        while i < nums.count - 1{
+            if nums[i] == nums[i + 1] {
+                nums.remove(at: i)
+            } else {
+                i += 1
+            }
+        }
+        
+        return nums.count
+    }
+    
 }
 
 
