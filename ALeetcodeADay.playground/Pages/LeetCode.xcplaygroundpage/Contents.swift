0dde34fef01485876498909d1dd2d0adf2ca28e1
evan0322 <evan201314@gmail.com>
2018-12-03 22:32:48 -0500

    88. Merge Sorted Array

--- ALeetcodeADay.playground/Pages/LeetCode.xcplaygroundpage/Contents.swift
+++ ALeetcodeADay.playground/Pages/LeetCode.xcplaygroundpage/Contents.swift
@@ -5974,6 +5974,35 @@ class Solution {
             }
         }
     }
+    
+    //88. Merge Sorted Array
+
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var i = m - 1
+        var j = n - 1
+        
+        var k = m + n - 1
+        
+        while i >= 0 && j >= 0 {
+            if nums1[i] > nums2[j] {
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            } else {
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+            }
+        }
+        
+        
+        while j >= 0 {
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
+        }
+        
+    }
 }
 
 
