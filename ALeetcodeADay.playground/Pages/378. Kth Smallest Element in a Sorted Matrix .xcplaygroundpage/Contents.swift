83aa41bd9eef7b6f7e5b1a4ab25b1e7f001a85b0
evan0322 <evan201314@gmail.com>
2018-12-26 17:16:54 -0500

    PriorityQueue

--- ALeetcodeADay.playground/Pages/378. Kth Smallest Element in a Sorted Matrix .xcplaygroundpage/Contents.swift
+++ ALeetcodeADay.playground/Pages/378. Kth Smallest Element in a Sorted Matrix .xcplaygroundpage/Contents.swift
@@ -2,8 +2,8 @@
 import Foundation
 
 class Solution {
-    
-     //Do a binary search, to find the num of elements that less than target num. If we find that k - 1 elements are less than target, then we find the answer
+    //378. Kth Smallest Element in a Sorted Matrix
+    //Do a binary search, to find the num of elements that less than target num. If we find that k - 1 elements are less than target, then we find the answer
     func kthSmallest(_ matrix: [[Int]], _ k: Int) -> Int {
         var lo = matrix[0][0]
         var hi = matrix[matrix.count - 1][matrix[0].count - 1] + 1
