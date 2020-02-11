ffe529c279370da608429aa6990f822a192fe812
evan0322 <evan201314@gmail.com>
2020-02-11 11:35:41 -0500

    update comment

--- ALeetcodeADay.playground/Pages/134. Gas Station .xcplaygroundpage/Contents.swift
+++ ALeetcodeADay.playground/Pages/134. Gas Station .xcplaygroundpage/Contents.swift
@@ -2,6 +2,17 @@
 
 import Foundation
 
+/*
+ Assumptions
+ 1. If total gas is more than total consumption, there is always a solution.
+ 2. If station i cannot reach station j, any node in [i + 1..<j] cannot reach j.
+ 
+ We start from station 0. If tank[i] < 0, then:
+ 1. Any station in [0..<i] we have already checked. They does not reach a certain station
+ 2. Any station in [i..<j] cannot reach j.
+ 
+ --> the answer is not in [0..<j]
+*/
 func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
      guard gas.count == cost.count, gas.count > 0 else {
          return -1
