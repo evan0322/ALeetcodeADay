43d01367e3d6864037ff03eea19cdbabf5d01a82
evan0322 <evan201314@gmail.com>
2020-04-30 09:56:28 -0400

    Update

--- ALeetcodeADay.playground/Pages/994. Rotting Oranges .xcplaygroundpage/Contents.swift
+++ ALeetcodeADay.playground/Pages/994. Rotting Oranges .xcplaygroundpage/Contents.swift
@@ -3,53 +3,51 @@
 import Foundation
 
 func orangesRotting(_ grid: [[Int]]) -> Int {
-    var count = 0
-    var stack = [[Int]]()
-    var fresh = 0
+   guard grid.count > 0, grid[0].count > 0 else {
+       return 0
+   }
+    
+    var freshOranges = 0
     var grid = grid
+    var minutes = 0
+    var stack = [[Int]]()
     
     for i in 0..<grid.count {
         for j in 0..<grid[i].count {
-            if grid[i][j] == 2 {
+            if grid[i][j] == 1 {
+                freshOranges += 1
+            } else if grid[i][j] == 2 {
                 stack.append([i, j])
-            } else if grid[i][j] == 1 {
-                fresh += 1
             }
-            
         }
     }
     
-    func bfsOnce(stack:[[Int]]) -> [[Int]] {
-        var dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        var result = [[Int]]()
-        for point in stack {
-            for dir in dirs {
-                let i = point[0] + dir[0]
-                let j = point[1] + dir[1]
-                if i < 0 || i >= grid.count || j < 0 || j >= grid[i].count {
-                    continue
-                }
-            
-                if grid[i][j] == 0 || grid[i][j] == 2 {
-                    continue
-                }
-            
-                grid[i][j] = 2
-                fresh -= 1
-                result.append([i, j])
-            }
-        }
-    
-        return result
-    }
+    let dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
     
     while stack.count != 0 {
-        stack =  bfsOnce(stack:stack)
+        var newStack = [[Int]]()
+        for node in stack {
+            for dir in dirs {
+                let i = node[0] + dir[0]
+                let j = node[1] + dir[1]
+                if i < 0 || j < 0 || i >= grid.count || j >= grid[i].count {
+                    continue
+                }
+                
+                if grid[i][j] == 1 {
+                    grid[i][j] = 2
+                    newStack.append([i, j])
+                    freshOranges -= 1
+                }
+            }
+        }
+        
+        stack = newStack
         if stack.count > 0 {
-            count += 1
+            minutes += 1
         }
     }
     
-    return fresh == 0 ? count : -1
+    return freshOranges == 0 ? minutes : -1
             
 }
