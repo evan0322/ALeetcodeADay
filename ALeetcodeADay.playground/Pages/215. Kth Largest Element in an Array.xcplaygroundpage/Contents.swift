3b5d07bfd2d13e5899b17688c9bbe0a467f6867f
evan0322 <evan201314@gmail.com>
2020-05-08 11:49:38 -0400

    215. Kth Largest Element in an Array

--- ALeetcodeADay.playground/Pages/215. Kth Largest Element in an Array.xcplaygroundpage/Contents.swift
+++ ALeetcodeADay.playground/Pages/215. Kth Largest Element in an Array.xcplaygroundpage/Contents.swift
@@ -3,49 +3,55 @@
 import Foundation
 
 //215. Kth Largest Element in an Array
+//use random pivot to ensure that time complexity is good with special test case
 func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-    guard nums.count > 0 else {
-        return 0
+    guard nums.count > 0, k > 0 else {
+        return -1
     }
     
+    var index = -1
     var nums = nums
-    let result = quickSort(nums:&nums, low:0, high:nums.count - 1, target: k)
-    if result == -1 {
-        return nums[nums.count - k]
-    } else {
-        return result
-    }
-}
-
-
-func quickSort(nums: inout [Int], low: Int, high: Int, target: Int) -> Int {
-    if low < high {
-        let pivot = partition(nums:&nums, low:low, high:high)
-        if pivot == nums.count - target {
-            return nums[pivot]
+    /*
+    [3, 2, 1, 5, 6, 4]
+    [1, 2, 3, 4, 5, 6]
+    k = 2
+    
+    nums.count - k
+    */
+    func quickSelect(nums:inout [Int], low:Int, high:Int) {
+        if low < high {
+            let pivot = partition(nums:&nums, low:low, high:high)
+            if pivot == nums.count - k {
+                index = pivot
+                return
+            } else if pivot > nums.count - k {
+                quickSelect(nums:&nums, low:low, high:pivot - 1)
+            } else {
+                quickSelect(nums:&nums, low:pivot + 1, high:high)
+            }
         }
+    }
+
+    func partition(nums:inout [Int], low:Int, high:Int) -> Int {
+        let index = Int.random(in: low...high)
+        let pivotVal = nums[index]
+        nums.swapAt(index,high)
         
-        if pivot > nums.count - target {
-            return quickSort(nums:&nums, low:low, high: pivot - 1, target:target)
-        } else {
-            return quickSort(nums:&nums, low:pivot + 1, high: high, target:target)
+        var i = low
+        for j in low..<high {
+            if nums[j] <= pivotVal {
+                nums.swapAt(i, j)
+                i += 1
+            }
         }
+
+        nums.swapAt(i, high)
+        return i
     }
+
+    quickSelect(nums:&nums,low:0, high:nums.count - 1)
     
-    return -1
+    return index == -1 ? nums[nums.count - k] : nums[index]
 }
 
-func partition(nums: inout [Int], low: Int, high: Int) -> Int {
-    let pivot = nums[high]
-    var i = low
-    
-    for j in i..<high {
-        if nums[j] <= pivot {
-            nums.swapAt(i, j)
-            i += 1
-        }
-    }
-    
-    nums.swapAt(i, high)
-    return i
-}
+
