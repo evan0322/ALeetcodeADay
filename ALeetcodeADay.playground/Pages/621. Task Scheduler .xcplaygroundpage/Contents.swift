e983d7d4a449e534c177247862530c10d8befd8d
evan0322 <evan201314@gmail.com>
2020-05-12 22:42:55 -0400

    Update 621. Task Scheduler

--- ALeetcodeADay.playground/Pages/621. Task Scheduler .xcplaygroundpage/Contents.swift
+++ ALeetcodeADay.playground/Pages/621. Task Scheduler .xcplaygroundpage/Contents.swift
@@ -2,43 +2,42 @@
 
 import Foundation
 /*
- Take [A, A, A, B, B, B] for example. We first find the task that apears most frequently,
- Build the task : A * * A * * A * *.  Between A * * A * * A, if the taks appears less than A, then we are sure that we can put the task in  the slots without conflicting the cool down. However, If B appears the same time as A, then B must be put outside of A * * A * * A, then make it A B * A B * A B
- We call everying after the last A tail, the length of tail is equal to the number of tasks that appears the same frequence as the most frequent task.
- After the tail is calculated and restriction is set, if the rest of the task is larger or eaqual in the slots, then basically the length of the taks is a parlindrome of the task itself, then return tasks.count. If  the task is less than the slots, then we need to calculate the length based on the slots.
+ First count the number of occurrences of each element.
+ Let the max frequency seen be M for element E
+ We can schedule the first M-1 occurrences of E, each E will be followed by at least N CPU cycles in between successive schedules of E
+ Total CPU cycles after scheduling M-1 occurrences of E = (M-1) * (N + 1) // 1 comes for the CPU cycle for E itself
+ Now schedule the final round of tasks. We will need at least 1 CPU cycle of the last occurrence of E. If there are multiple tasks with frequency M, they will all need 1 more cycle.
+ Run through the frequency dictionary and for every element which has frequency == M, add 1 cycle to result.
+ If we have more number of tasks than the max slots we need as computed above we will return the length of the tasks array as we need at least those many CPU cycles.
+ 
  */
-
-func leastInterval(_ tasks: [Character], _ n: Int) -> Int {
-    guard tasks.count > 0 else {
-        return 0
-    }
-    
-    var memo = [Character:Int]()
-    var max = 0
-    var mChar: Character = "*"
-    var tail = 0
-    
-    for t in tasks {
-        memo[t] = memo[t, default: 0] + 1
-        if memo[t]! > max {
-            mChar = t
-            max = memo[t]!
-        }
-    }
-    
-    for key in memo.keys {
-        if memo[key]! == max {
-            tail += 1
-        }
-    }
-    
-    tail -= 1
-    let slots = (max - 1) * n
-    
-    if tasks.count - max - tail > slots {
-        return tasks.count
-    } else {
-        return slots + max + tail
-    }
+ func leastInterval(_ tasks: [Character], _ n: Int) -> Int {
+     guard tasks.count > 0 else {
+         return 0
+     }
+     
+     var memo = [Character:Int]()
+     var maxCount = 1
+     
+     for task in tasks {
+         if var count = memo[task] {
+             count += 1
+             maxCount = max(maxCount, count)
+             memo[task] = count
+         } else {
+             memo[task] = 1
+         }
+     }
+     
+     var result = (maxCount - 1) * (n + 1)
+     
+     for (key, value) in memo {
+         if value == maxCount {
+             result += 1
+         }
+     }
+     
+     return max(result, tasks.count)
+ }
     
 }
